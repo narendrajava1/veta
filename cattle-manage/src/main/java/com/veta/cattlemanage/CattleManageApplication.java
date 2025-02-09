4c95def2030e0b81bd8f647a38484f8a8e42b541
narendra <kolli7571@gmail.com>
2025-02-09 12:13:30 +0530

    added cattele-maange module

---
+++ cattle-manage/src/main/java/com/veta/cattlemanage/CattleManageApplication.java
@@ -0,0 +1,13 @@
+package com.veta.cattlemanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CattleManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CattleManageApplication.class, args);
+    }
+
+}
---
+++ cattle-manage/src/test/java/com/veta/cattlemanage/CattleManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veta.cattlemanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CattleManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
